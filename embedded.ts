c93aedd54ad8fbb598be1ef42b2f381b32936516
Anton Vorobiov <vorobiov.anton@icloud.com>
2024-05-19 17:01:13 +0300

    Add listening to serial port and saving values to Postgres

---
+++ embedded.ts
@@ -0,0 +1,34 @@
+import { SerialPort, ReadlineParser } from "serialport"
+import env from "dotenv"
+import { PrismaClient } from "@prisma/client"
+
+env.config()
+
+const parser = new ReadlineParser()
+
+const prisma = new PrismaClient()
+
+const port = new SerialPort({ 
+  path: process.env.PORT!, 
+  baudRate: Number(process.env.BAUD_RATE!),
+})
+
+port.pipe(parser)
+
+interface AirMetricInterface {
+  humidity: number
+  temperature: number
+  createdAt: number
+}
+
+type NativeAirMetric = Omit<AirMetricInterface, "createdAt">
+
+parser.on('data', async (values) => {
+  const nativeAirMetric = JSON.parse(values) as NativeAirMetric
+  
+  console.log('Writing to pg', nativeAirMetric);
+
+  await prisma.airMetric.create({
+    data: nativeAirMetric,
+  })
+})
